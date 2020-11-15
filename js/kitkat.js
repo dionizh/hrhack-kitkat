8406a29255d5077c9ee32c68b398d4d19cfe9a94
Dioni Zhong <dioniys@yahoo.com>
2020-11-15 16:44:39 +0000

    shorten timer

--- js/kitkat.js
+++ js/kitkat.js
@@ -4,10 +4,10 @@ $(document).ready(function() {
     document.getElementById('bubble3').style.display = "none";
     document.getElementById('bubble4').style.display = "none";
 
-    setTimeout(showBubble1, 5000);
-    setTimeout(showBubble2, 7000);
-    setTimeout(showBubble3, 9000);
-    setTimeout(showBubble4, 10000);
+    setTimeout(showBubble1, 3000);
+    setTimeout(showBubble2, 5000);
+    setTimeout(showBubble3, 7000);
+    setTimeout(showBubble4, 8000);
 });
 
 function showBubble1() {
