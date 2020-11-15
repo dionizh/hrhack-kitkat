4b5afd9eb22e40799b04d54e1b36cbfb6929797e
Dioni Zhong <dioniys@yahoo.com>
2020-11-15 19:19:24 +0000

    add video links and rearrange bubbles

--- js/kitkat.js
+++ js/kitkat.js
@@ -4,17 +4,16 @@ $(document).ready(function() {
     document.getElementById('bubble3').style.display = "none";
     document.getElementById('bubble4').style.display = "none";
 
-    setTimeout(showBubble1, 3000);
-    setTimeout(showBubble2, 5000);
-    setTimeout(showBubble3, 7000);
-    setTimeout(showBubble4, 8000);
+    setTimeout(showBubble1, 5000);    
 });
 
 function showBubble1() {
     document.getElementById('bubble1').style.display = "block";
 }
 function showBubble2() {
-    document.getElementById('bubble2').style.display = "block";
+    document.getElementById('bubble2').style.display = "block";    
+    setTimeout(showBubble3, 2000);
+    setTimeout(showBubble4, 4000);
 }
 function showBubble3() {
     document.getElementById('bubble3').style.display = "block";
